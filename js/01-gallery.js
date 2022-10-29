1361bbc7658193310f1147b45a9d0bc1176591c4
TheRealGio <42873152+TheRealGio@users.noreply.github.com>
2022-10-29 18:23:48 +0300

    HW#7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleyDiv = document.querySelector(".gallery");
+
+galleryItems.map(({ preview, original, description }) => {
+  galleyDiv.insertAdjacentHTML(
+    "beforeend",
+    `<div class="gallery__item"><a class="gallery__link" href="${original}"><img loading="lazy" class="gallery__image" data-src="${original}" alt="${description}" src="${preview}""/></a></div>`
+  );
+});
+
+const onPictureClick = (event) => {
+  event.preventDefault();
+  const picSource = event.target.dataset.src;
+  const modal = basicLightbox.create(
+    `<img src="${picSource}" width="800" height="600">`
+  );
+
+  modal.show();
+};
+
+galleyDiv.addEventListener("click", onPictureClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleyDiv = document.querySelector(".gallery");
+
+galleryItems.map(({ preview, original, description }) => {
+  galleyDiv.insertAdjacentHTML(
+    "beforeend",
+    `<li><a class="gallery__link" href="${original}"><img loading="lazy" class="gallery__image" data-src="${original}" alt="${description}" src="${preview}""/></a></li>`
+  );
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
